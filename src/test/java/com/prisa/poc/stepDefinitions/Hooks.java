8d62376a20b3581dde849fc86e074fc714691be1
mkosakovskyy <mkosakovskyy@hiberus.com>
2022-10-19 14:44:29 +0200

    test no headless

--- src/test/java/com/prisa/poc/stepDefinitions/Hooks.java
+++ src/test/java/com/prisa/poc/stepDefinitions/Hooks.java
@@ -47,11 +47,12 @@ public class Hooks {
                 System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
                 WebDriverManager.chromedriver().setup();
                 ChromeOptions optionsChrome = new ChromeOptions();
-                optionsChrome.addArguments("--headless");
+                // optionsChrome.addArguments("--headless");
                 optionsChrome.addArguments("--no-sandbox");
                 optionsChrome.addArguments("--disable-dev-shm-usage");
                 optionsChrome.addArguments("--disable-web-security");
                 optionsChrome.addArguments("--enable-javascript");
+                optionsChrome.addArguments("--remote-debugging-port=9222");
                 driver = new ChromeDriver(optionsChrome);
         }
         driver.manage().timeouts().implicitlyWait(TIMEOUT, TimeUnit.SECONDS);
